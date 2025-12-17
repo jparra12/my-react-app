e6f5633045a5959ec1acb8bc60086bed0b1c6a30
jparra12 <parrajesusalejandro@gmail.com>
2025-12-17 05:35:27 -0400

    setting static elements

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,30 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
+//import './index.css';
 
 
 createRoot(document.getElementById('root')).render(
-<h1>hahaha</h1>
+<>
+<header>
+<img src="react-logo.png" width='40px' alt="React Logo" />
+<nav>
+  <ul>
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+  </ul>
+</nav>
+</header>
+<main>
+ <h1>Reasons I am excited to learn React</h1>
+ <ul>React is a popular library, so I will be able to fit in with all the coolest devs out there</ul>
+ <ul>I'm more likely to get a job as a developer if I know React</ul>
+</main>
+<footer>
+ <p>2024 Ziroll Development. All rights reserved.</p>
+
+</footer>
+
+
+
+</>
 )
