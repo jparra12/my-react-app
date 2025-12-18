c183a6272a2374a1445887231030404a13681c8c
jparra12 <parrajesusalejandro@gmail.com>
2025-12-18 12:15:05 -0400

    reorganizing components

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+  return(
+    <header>
+      <img src="react-logo.png" className="nav-logo" alt="React Logo" />
+      <nav>
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ src/components/main.jsx
@@ -0,0 +1,11 @@
+export default function Main(){
+  return(
+    <main>
+      <h1>Reasons I am excited to learn React</h1>
+      <ol>
+        <li>React is a popular library, so I will be able to fit in with all the coolest devs out there</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </main>
+  )
+}
